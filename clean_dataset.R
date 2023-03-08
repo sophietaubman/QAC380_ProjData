c913766ef392e0447e41a2137e0c368b9324b77f
Miles Lambke <mlambke@wesleyan.edu>
2023-03-08 08:48:14 -0500

    loading dataset

---
+++ clean_dataset.R
@@ -0,0 +1,2 @@
+MASTER_CLEAN_DATA_FOTM <- read_excel("/Volumes/qac380/Data and Codebooks 2023/RIPHI/FOTM Data/MASTER CLEAN DATA FOTM.xlsx")
+setwd("/Users/mileslambke/Desktop/college/courses/statistical_consulting/QAC380_ProjData")
