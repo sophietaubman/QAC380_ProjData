6de27676c077f0131e9733f04b89d025039fecfd
sophietaubman <124796799+sophietaubman@users.noreply.github.com>
2023-03-27 08:55:15 -0400

    r data management

---
+++ ProjData.R
@@ -0,0 +1,33 @@
+# Uploading Data Set
+MASTER_CLEAN_DATA_FOTM <- read_excel("~/Downloads/MASTER CLEAN DATA FOTM.xlsx")
+View(MASTER_CLEAN_DATA_FOTM)
+
+# Load libraries needed
+
+library(descr)
+library(ggplot2)
+
+# DATA MANAGEMENT #
+
+# Variables: 
+  # unique ID = loyalty number
+  # Physical
+  # mental health: 
+  # snap status: Y/N 
+  # employment status: Y/N 
+  # Retired: Y/N
+  # ethnicity: Hispanic Y/N
+  # race demographics: American Indian or Alaska Native, Asian, Black or African American, Native Hawaiian or Other Pacific Islander, White ore Caucasian, Oteher Race
+
+# Make New Subset of Data Using Variables of Interest:
+
+FOTM_BL_Sub <- MASTER_CLEAN_DATA_FOTM[,c("Loyalty card number","Age", "GENDER","RACE","HISPANIC","INSURANCEYN","MEDICARE","MEDICAID","EMPLOYED","RETIRED","DISABLED","SNAP","GENHEALTH","HEALTH_PHYS_0_TEXT","HEALTH_MENTAL_0_TEXT")]
+View(FOTM_BL_Sub)
+
+# Renaming Variables:
+
+names(FOTM_BL_Sub)[names(FOTM_BL_Sub)== "HEALTH_PHYS_0_TEXT"] <- "HEALTH_PHYS"
+names(FOTM_BL_Sub)[names(FOTM_BL_Sub)== "HEALTH_MENTAL_0_TEXT"] <- "HEALTH_MENTAL"
+
+
+
