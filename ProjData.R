750a575a8127033c5211e9009e1095897a6a4548
Miles Lambke <mlambke@wesleyan.edu>
2023-03-28 11:37:04 -0400

    Tuesday Morn Percentage Summaries
    Since our working directories are all different just remember to alter the read_excel command for whatever pulls the MASTER_CLEAN_DATA correctly, think the one in here is from me and may not work for you guys

--- ProjData.R
+++ ProjData.R
@@ -1,8 +1,5 @@
-################################################################################
-# Uploading Data Set 
-################################################################################
-
-MASTER_CLEAN_DATA_FOTM <- read_excel("~/Downloads/MASTER CLEAN DATA FOTM.xlsx")
+# Uploading Data Set
+MASTER_CLEAN_DATA_FOTM <- read_excel("/Volumes/qac380/Data and Codebooks 2023/RIPHI/FOTM Data/MASTER CLEAN DATA FOTM.xlsx")
 View(MASTER_CLEAN_DATA_FOTM)
 
 ################################################################################
@@ -17,22 +14,22 @@ library(ggplot2)
 ################################################################################
 
 # Variables: 
-  # Unique ID ("Loyalty card number")
-  # Age ("Age")
-  # Gender ("GENDER") "Man", "Woman", or "Genderqueer/non-binary, neither exclusively man nor woman"
-  # Race** ("RACE") American Indian or Alaska Native, Asian, Black or African American, 
-      # Native Hawaiian or Other Pacific Islander, White ore Caucasian, Oteher Race **Option to select multiple
-  # Hispanic ("HISPANIC") YES/NO
-  # Insurance ("INSURANCEYN") YES/NO
-  # Medicare ("MEDICARE") YES/NO
-  # Medicaid ("MEDICAID") YES/NO
-  # Employed ("EMPLOYED") YES/NO/NA
-  # Retired ("RETIRED") YES/NO/NA
-  # Disabled ("DISABLED") YES/NO
-  # Snap ("SNAP") YES/NO
-  # General Health ("GENHEALTH") "Poor" = 1, "Fair, or" = 2, "Good" = 3, "Very good" = 4, "Excellent" = 5
-  # Physical Health ("HEALTH_PHYS") Number of days of poor physical health in past 30 days
-  # Mental Health ("HEALTH_MENTAL") Number of days of poor mental health in past 30 days
+# Unique ID ("Loyalty card number")
+# Age ("Age")
+# Gender ("GENDER") "Man", "Woman", or "Genderqueer/non-binary, neither exclusively man nor woman"
+# Race** ("RACE") American Indian or Alaska Native, Asian, Black or African American, 
+# Native Hawaiian or Other Pacific Islander, White ore Caucasian, Oteher Race **Option to select multiple
+# Hispanic ("HISPANIC") YES/NO
+# Insurance ("INSURANCEYN") YES/NO
+# Medicare ("MEDICARE") YES/NO
+# Medicaid ("MEDICAID") YES/NO
+# Employed ("EMPLOYED") YES/NO/NA
+# Retired ("RETIRED") YES/NO/NA
+# Disabled ("DISABLED") YES/NO
+# Snap ("SNAP") YES/NO
+# General Health ("GENHEALTH") "Poor" = 1, "Fair, or" = 2, "Good" = 3, "Very good" = 4, "Excellent" = 5
+# Physical Health ("HEALTH_PHYS") Number of days of poor physical health in past 30 days
+# Mental Health ("HEALTH_MENTAL") Number of days of poor mental health in past 30 days
 
 ################################################################################
 # Make New Subset of Data Using Variables of Interest
@@ -72,11 +69,11 @@ View(FOTM_BL_Sub_1)
 
 # AGE # 
 
-  # Univariate Bar Graph:
+# Univariate Bar Graph:
 
 ggplot(data=FOTM_BL_Sub_1)+geom_bar(aes(x=factor(Age)))+ggtitle("Age")
 
-  # Univariate Histogram: 
+# Univariate Histogram: 
 
 ggplot(FOTM_BL_Sub_1, aes(Age)) + geom_histogram(binwidth = 5) + ggtitle("Age Distribution") + 
   ylab("Number of People")
@@ -88,11 +85,11 @@ ggplot(FOTM_BL_Sub_1, aes(Age)) + geom_histogram(binwidth = 5) + ggtitle("Age Di
 # Bar graph ordered by number of poor health days 0 to 30
 
 ggplot(data=FOTM_BL_Sub_1) + geom_bar(aes(x=factor(HEALTH_PHYS, level=c
-                                                 ("0","1","2","3","4","5","6","7","8","9","10",
-                                                   "11","12","13","14","15","16","17","18","19","20",
-                                                   "21","22","23","24","25","26","27","28","29","30")))) + 
+                                                   ("0","1","2","3","4","5","6","7","8","9","10",
+                                                     "11","12","13","14","15","16","17","18","19","20",
+                                                     "21","22","23","24","25","26","27","28","29","30")))) + 
   xlab("# of Days") + ylab("# of People") + ggtitle("# of Days of Poor Health in Past 30 Days")
-  
+
 ################################################################################
 
 # MENTAL HEALTH #
@@ -167,12 +164,28 @@ ggplot(data=FOTM_BL_Sub_1)+geom_bar(aes(x=factor(DISABLED)))+ggtitle("Disabled")
 ggplot(data=FOTM_BL_Sub_1)+geom_bar(aes(x=factor(GENHEALTH)))+ggtitle("General Health Rating") + xlab("") + ylab("# of People") 
 
 ################################################################################
+# PERCENTAGE SUMMARY FOR DATAFRAME LEVELS #
+# gender 
+FOTM_BL_Sub_1 %>% group_by(GENDER) %>% summarise(Percentage=n()/nrow(.))
+# race 
+FOTM_BL_Sub_1 %>% group_by(RACE) %>% summarise(Percentage=n()/nrow(.))
+# hispanic 
+FOTM_BL_Sub_1 %>% group_by(HISPANIC) %>% summarise(Percentage=n()/nrow(.))
+# insurance
+FOTM_BL_Sub_1 %>% group_by(INSURANCEYN) %>% summarise(Percentage=n()/nrow(.))
+# medicare
+FOTM_BL_Sub_1 %>% group_by(MEDICARE) %>% summarise(Percentage=n()/nrow(.))
+# medicaid
+FOTM_BL_Sub_1 %>% group_by(MEDICAID) %>% summarise(Percentage=n()/nrow(.))
+# employment status
+FOTM_BL_Sub_1 %>% group_by(EMPLOYED) %>% summarise(Percentage=n()/nrow(.))
+# retirement
+FOTM_BL_Sub_1 %>% group_by(RETIRED) %>% summarise(Percentage=n()/nrow(.))
+# disability
+FOTM_BL_Sub_1 %>% group_by(DISABLED) %>% summarise(Percentage=n()/nrow(.))
+# snap
+FOTM_BL_Sub_1 %>% group_by(SNAP) %>% summarise(Percentage=n()/nrow(.))
+# genhealth
+FOTM_BL_Sub_1 %>% group_by(GENHEALTH) %>% summarise(Percentage=n()/nrow(.))
 
-
-
-
-
-
-
-
-
+################################################################################
