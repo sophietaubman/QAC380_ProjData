37c04519ca0dcef961cf4a555fbf1dca3e0473b0
Miles Lambke <mlambke@wesleyan.edu>
2023-04-18 22:01:28 -0400

    4.18 LCA code

--- LCAcode.R
+++ LCAcode.R
@@ -123,23 +123,28 @@ LCA_subset$RETIRED[LCA_subset$RETIRED == "DON'T KNOW"|LCA_subset$RETIRED == "REF
 
 LCA_subset$HUNGRY[LCA_subset$HUNGRY == "DON'T KNOW"|LCA_subset$HUNGRY == "REFUSED"] <- NA
 
+# INSURANCEYN #
+
+LCA_subset$INSURANCEYN[LCA_subset$INSURANCEYN == "DON'T KNOW"] <- NA
+
 
 ################################################################################
 # Recode response values to numbers starting at "1" (It's a poLCA thing)
 ################################################################################
 
 LCA_subset$Age<-revalue(LCA_subset$Age, c("26_63"="1", "63_71"="2", "71_78"="3", "78up"="4"))
-LCA_subset$INSURANCEYN<-LCA_subset(lca_subset$INSURANCEYN, c("YES"="1", "NO"="2"))
+LCA_subset$INSURANCEYN<-revalue(LCA_subset$INSURANCEYN, c("NO"="1", "YES"="2"))
 LCA_subset$DISABLED<-revalue(LCA_subset$DISABLED, c("NO"="1", "YES"="2"))
 LCA_subset$SNAP<-revalue(LCA_subset$SNAP, c("NO"="1", "YES"="2"))
 LCA_subset$GENHEALTH<-revalue(LCA_subset$GENHEALTH, c("Poor"="1", "Fair"="2", "Good"="3", "Very good"="4", "Excellent"="5"))
-LCA_subset$GENDER<-revalue(LCA_subset$GENDER, c("Male"="1", "Woman"="2", "Genderqueer"="3"))
+LCA_subset$GENDER<-revalue(LCA_subset$GENDER, c("Man"="1", "Woman"="2", "Genderqueer"="3"))
 LCA_subset$HISPANIC<-revalue(LCA_subset$HISPANIC, c("NO"="1", "YES"="2"))
 LCA_subset$RACE<-revalue(LCA_subset$RACE, c("American Indian"="1", "Black"="2",
                                             "Cape Verdan"="3", "Dominican"="4", "Hispanic"="5",
                                             "Latina"="6", "Puerto Rican"="7", "Spanish"="8", "White"="9",
                                             "Other"="10"))
-### continue at RETIRED ### -miles
+LCA_subset$RETIRED<-revalue(LCA_subset$RETIRED, c("Employed"="1", "Unemployed"="2", "Retired"="3"))
+LCA_subset$HUNGRY<-revalue(LCA_subset$HUNGRY, c("NO"="1", "YES"="2"))
 
 
 ################################################################################
@@ -152,6 +157,11 @@ LCA_subset$INSURANCEYN<-as.factor(LCA_subset$INSURANCEYN)
 LCA_subset$DISABLED<-as.factor(LCA_subset$DISABLED)
 LCA_subset$SNAP<-as.factor(LCA_subset$SNAP)
 LCA_subset$GENHEALTH<-as.factor(LCA_subset$GENHEALTH)
+LCA_subset$GENDER<-as.factor(LCA_subset$GENDER)
+LCA_subset$HISPANIC<-as.factor(LCA_subset$HISPANIC)
+LCA_subset$RACE<-as.factor(LCA_subset$RACE)
+LCA_subset$RETIRED<-as.factor(LCA_subset$RETIRED)
+LCA_subset$HUNGRY<-as.factor(LCA_subset$HUNGRY)
 
 ################################################################################
 # Define the LCA formula. Variables in parentheses are the latent class classification variables. 
@@ -159,7 +169,7 @@ LCA_subset$GENHEALTH<-as.factor(LCA_subset$GENHEALTH)
 # Run the LCA specifying a range of classes
 ################################################################################
 
-f <- cbind(Age, INSURANCEYN, DISABLED, SNAP)~GENHEALTH
+f <- cbind(Age, INSURANCEYN, DISABLED, SNAP, GENDER, HISPANIC, RACE, RETIRED)~GENHEALTH+HUNGRY
 
 ################################################################################
 # Latent Class Analysis Specifying 1-3 Classes
@@ -169,6 +179,7 @@ lCA1 <- poLCA(f,LCA_subset, nclass=1,nrep=15)
 lCA2 <- poLCA(f,LCA_subset, nclass=2,nrep=15, graphs = T)
 lCA3 <- poLCA(f,LCA_subset, nclass=3,nrep=15, graphs = T)
 lCA4 <- poLCA(f,LCA_subset, nclass=4,nrep=15, graphs = T)
+
 
 ################################################################################
 # THIS (ENTROPY) WILL HAPPEN LATER #
